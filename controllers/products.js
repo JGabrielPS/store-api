83e04d113fdb3a2bb4abf97766241bf65083b95b
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2022-06-22 12:16:08 -0300

    feat: add logic to find all products in static get all static endpoint. Also add the query of data based in the fields featured, company and name, this with regex. Finally, add sort logic to the query

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,24 @@
 import { createCustomError } from "../errors/custom-error.js";
+import Product from "../models/product.json";
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const products = await Product.find({});
+  res.status(200).json({ nbHits: products.length });
 };
 
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "static products route" });
+  const { featured, company, name, sort } = req.query;
+  let objectQuery = {};
+  if (featured) objectQuery.featured = featured || false;
+  if (company) objectQuery.company = company;
+  if (name) objectQuery.name = { $regex: name, $options: "i" };
+  let result = Product.find(objectQuery);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = products.sort(sortList);
+  } else result = result.sort("createAt");
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 export { getAllProducts, getAllProductsStatic };
